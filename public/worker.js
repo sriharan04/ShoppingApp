95d5bffc561c6903c4893fa57b01b22a76596fac
sriharan04 <41664691+sriharan04@users.noreply.github.com>
2020-04-20 17:32:26 +0530

    worker modification Commit

--- public/worker.js
+++ public/worker.js
@@ -1,44 +1,47 @@
-var CACHE_NAME = "shoppify-cache";
-var urlToCache = [
+var CACHE_NAME = "pwa-task-manager";
+var urlsToCache = [
   "/",
   "/favicon.io",
-  "/index.html",
-  "/mainfest.json",
-  "/service-worker",
-  "/products.json",
+  "index.html",
+  "mainfest.json",
+  "service-worker.js",
+  "products.json",
 ];
-
 self.addEventListener("install", (event) => {
+  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlToCache))
-  );
-});
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames
-          .filter(function (cacheName) {})
-          .map(function (cacheName) {
-            return caches.delete(cacheName);
-          })
-      );
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Cache and return requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(function (response) {
+      // Cache hit - return response
       if (response) {
-        console.log("Found in cache");
-        console.log(event.request);
-        console.log(response);
         return response;
       }
-      console.log("Not found in cache");
-      console.log(event.request);
       return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["pwa-task-manager"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
